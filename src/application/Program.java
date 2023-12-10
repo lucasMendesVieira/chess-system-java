b035928cb974850449da1374e4861d0707ed7e2c
Lucas Mendes <lmvieiratv@gmail.com>
2023-12-10 15:33:44 -0300

    Possible moves of a piece

--- src/application/Program.java
+++ src/application/Program.java
@@ -11,34 +11,32 @@ import chess.ChessPosition;
 public class Program {
 
 	public static void main(String[] args) {
-		
+
 		Scanner sc = new Scanner(System.in);
 		ChessMatch chessMatch = new ChessMatch();
-		
-		while(true) {
+
+		while (true) {
 			try {
-			
-			UI.clearScreen();
-			UI.printBoard(chessMatch.getPieces());
-			System.out.println();
-			System.out.print("source: ");
-			ChessPosition source = UI.readChessPosition(sc);
-			
-			System.out.println();
-			System.out.print("Target: ");
-			ChessPosition target = UI.readChessPosition(sc);
-			
-			ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
-	
+
+				UI.clearScreen();
+				UI.printBoard(chessMatch.getPieces());
+				System.out.println();
+				System.out.print("source: ");
+				ChessPosition source = UI.readChessPosition(sc);
+
+				System.out.println();
+				System.out.print("Target: ");
+				ChessPosition target = UI.readChessPosition(sc);
+
+				ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+
+			} catch (ChessException e) {
+				System.out.println(e.getMessage());
+				sc.nextLine();
+			} catch (BoardException e) {
+				System.out.println(e.getMessage());
+				sc.nextLine();
 			}
-		catch(ChessException e) {
-			System.out.println(e.getMessage());
-			sc.nextLine();
 		}
-		catch(BoardException e) {
-			System.out.println(e.getMessage());
-			sc.nextLine();
-		}
-	}
 	}
 }
